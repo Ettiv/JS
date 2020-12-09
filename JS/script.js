00336a57b9bc6b62a955322eeaa9586903757133
Victor <victor.grudanov@gmail.com>
2020-12-09 22:06:40 +0300

    завершил 12 урок

--- JS/script.js
+++ JS/script.js
@@ -3,5 +3,22 @@
 
 let numberOfFilms = prompt("Сколько фильмов вы посмотреои?");
 
-console.log(numberOfFilms);
+let personalMuvieDB = {
+    count: numberOfFilms,
+    movies: { 
+       
+    },
+    actors: {},
+    genres:[],
+    privat:false
+};
+
+
+const a = prompt("Один из последних просмотренных фильмов" , "" ),
+      b = prompt("Какую оцнку вы бы ему поставили?" , ""),
+      c = prompt("Один из последних просмотренных фильмов" , ""),
+      d = prompt("Какую оцнку вы бы ему поставили?" , "");
+personalMuvieDB.movies[a] = b;
+personalMuvieDB.movies[c] = d;
+console.log(personalMuvieDB);
 
