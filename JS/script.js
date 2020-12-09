b2338e9e05b822a923eec9d6e2242d9bbfdcfa11
Victor <victor.grudanov@gmail.com>
2020-12-09 21:14:52 +0300

    создал перемнную numberOfFilms

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,7 @@
 "use strict";
 
 
-let incr = 10,
-    decr = 10;
-//incr++;
-//decr--;
-console.log(incr++);
-console.log(decr--);
-console.log(++incr);
-console.log(--decr);
+let numberOfFilms = prompt("Сколько фильмов вы посмотреои?");
 
-console.log(2*2 == 4);
-console.log(2*2 == `4`);
-console.log(2*2 === `4`);
+console.log(numberOfFilms);
 
-const isChecked = false,
-      isClosed = false;
-
-console.log(isChecked || !isClosed);
