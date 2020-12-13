cc871ac98af4fc2019cd316cefbeb789614f3537
Victor <victor.grudanov@gmail.com>
2020-12-13 11:34:41 +0300

    3 практика

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,32 @@
 "use strict";
 
 
-let numberOfFilms = prompt("Сколько фильмов вы посмотреои?");
+let numberOfFilms;
+
+function start (){
+    numberOfFilms  = +prompt("Сколько фильмов вы посмотрели?");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms  = +prompt("Сколько фильмов вы посмотрели?");
+    }
+}
+
+function showMyDB (){
+    if (personalMuvieDB.privat == false){
+        console.log(personalMuvieDB);
+    } else{
+        console.log('не могу вывести приваиную информацию');
+    }
+}
+
+function writeYourGenres (){
+    for (let i =0; i< 3; i++){
+        let d = i+1;
+        personalMuvieDB.genres[i] = prompt('Ваш любимый жанр под номером ' + d);
+
+    }
+}
+start();
 
 let personalMuvieDB = {
     count: numberOfFilms,
@@ -14,45 +39,36 @@ let personalMuvieDB = {
 };
 
 
-let a , b;
-for (let i = 1; i < 3;i++){
+
+function rememberMyFilms(){
+    let a , b;
+    for (let i = 1; i < 3;i++){
     
-    a = prompt("Один из последних просмотренных фильмов" , "" );
-    b = prompt("Какую оцнку вы бы ему поставили?" , "");
-    if (a == "" || b == "" || a == null || b == null ||  a.length > 50 || b.length > 50){
+        a = prompt("Один из последних просмотренных фильмов" , "" );
+        b = prompt("Какую оцнку вы бы ему поставили?" , "");
+        if (a == "" || b == "" || a == null || b == null ||  a.length > 50 || b.length > 50){
         i--;
         continue;
-    }
+        }
     personalMuvieDB.movies[a] = b;
+    }
 }
-// let t = 1;
-// while (t < 3) {
-//     t++;
-//     a = prompt("Один из последних просмотренных фильмов" , "" );
-//     b = prompt("Какую оцнку вы бы ему поставили?" , "");
-//     if (a == "" || b == "" || a == null || b == null ||  a.length > 50 || b.length > 50){
-//         t--;
-//         continue;
-//     }
-//     personalMuvieDB.movies[a] = b;
 
-// } 
-// do {
-//     t++;
-//     a = prompt("Один из последних просмотренных фильмов" , "" );
-//     b = prompt("Какую оцнку вы бы ему поставили?" , "");
-//     if (a == "" || b == "" || a == null || b == null ||  a.length > 50 || b.length > 50){
-//         t--;
-//         continue;
-//     }
-//     personalMuvieDB.movies[a] = b;
-// } while (t<3);
-if ( personalMuvieDB.count < 10){
-    console.log("прсмотренно довольно мало фильмов");
-} else if (personalMuvieDB.count < 30) {
-    console.log('вы классический зритель');
-} else { 
-    console.log("вы киноман");
+rememberMyFilms();
+
+function detectPersonalLevel(){
+    if ( personalMuvieDB.count < 10){
+        console.log("прсмотренно довольно мало фильмов");
+    } else if (personalMuvieDB.count < 30) {
+        console.log('вы классический зритель');
+    } else { 
+        console.log("вы киноман");
+    }
 }
-console.log(personalMuvieDB);
+writeYourGenres();
+detectPersonalLevel();
+showMyDB();
+
+
+
 
