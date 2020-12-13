5276f745c741b8be9eda0bb0e23c16b81375152a
Victor <victor.grudanov@gmail.com>
2020-12-13 11:54:50 +0300

    шабблон колбэк функций

---
+++ CallBackFunction.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function first () {
+    //код
+    setTimeout(function(){
+        console.log(1);
+    } , 500);
+}
+
+function second () {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS (lang, callBack) {
+    console.log(`Я учу: ${lang}`);
+    callBack();
+}
+learnJS("java skript" , function(){
+    console.log('Я прошёл этот урок');
+});
+
+
+
