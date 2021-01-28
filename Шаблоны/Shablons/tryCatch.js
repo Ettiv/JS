766197060cf6515d0d7261883924e918bffbcb0d
Victor <victor.grudanov@gmail.com>
2021-01-28 20:35:00 +0300

    новые шаблоны

---
+++ Шаблоны/Shablons/tryCatch.js
@@ -0,0 +1,16 @@
+"use strict";
+
+try {
+    console.log("normal");
+    console.log(a);
+    console.log("result");
+} catch (error){
+    console.log(error);
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+} finally {
+    
+}
+
+console.log("still normal");
