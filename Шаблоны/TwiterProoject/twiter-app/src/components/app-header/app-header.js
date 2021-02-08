f276cbf4dbba1fb12773d4490afccc255d60e0a4
Victor <victor.grudanov@gmail.com>
2021-02-08 21:58:21 +0300

    Создание структуры приложения

---
+++ Шаблоны/TwiterProoject/twiter-app/src/components/app-header/app-header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import App from '../app/app';
+
+const AppHeader = () => {
+    return(
+        <div className = "app-header d-flex">
+            <h1>
+                Виктор Груданов
+            </h1>
+            <h2>
+                5 записей , из них понравилось 0
+            </h2>
+
+        </div>
+    );
+}
+
+export default AppHeader;
